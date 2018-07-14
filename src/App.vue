0a18bf8d6618b340c4e76a5b1a9ffdf709ab7edd
Manuel Alfaro Sierra <dokoto.moloko@gmail.com>
2018-07-14 18:58:26 +0200

    Ahi vamos---arriba, arriba git status

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app" class="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+body,
+html {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+body,
+input,
+textarea {
+  font-family: 'Poiret One', serif;
+  font-size: 16px;
+}
+
+.app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+</style>
